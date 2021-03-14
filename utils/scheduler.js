600d78aa49e3c0085255b581194be0c42b78e86b
zhangwang131627 <79137901+zhangwang131627@users.noreply.github.com>
2021-03-14 18:35:58 +0800

    Update scheduler.js

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -70,7 +70,7 @@ let scheduler = {
             let options = tasks[taskName].options || {}
             let willTime = moment(randomDate(options));
             // 任务的随机延迟时间
-            let waitTime = options.dev ? 0 : Math.floor(Math.random() * (options.waitTime || 100))
+            let waitTime = options.dev ? 0 : Math.floor(Math.random() * (options.waitTime || 20))
             if (options) {
                 if (options.isCircle || options.dev) {
                     willTime = moment().startOf('days');
